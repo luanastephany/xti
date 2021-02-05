6e435cd34ac1cb8381292cc58f3fdb5f8f7cf8a3
Luana <ls.luanastephany@gmail.com>
2021-02-05 05:38:16 -0300

    array simples

---
+++ js/Aula27/array.js
@@ -0,0 +1,10 @@
+
+var paises = ["Brasil", "Russia", "India", "China"]
+
+/*for (var i = 0; i < paises.length; i++)
+    alert(paises[i]) */
+
+alert(paises.sort()) //ordenar
+
+alert(paises.push("Canada")) //adiciona ao fim da fila
+alert(paises)
