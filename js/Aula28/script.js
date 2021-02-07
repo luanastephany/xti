cb9ce786170aa7c527bee7f15cfb61ad76c265fd
Luana <ls.luanastephany@gmail.com>
2021-02-07 10:24:28 -0300

    Array Básico

---
+++ js/Aula28/script.js
@@ -0,0 +1,24 @@
+/*
+var pessoas = [
+    ["Luana", "F"],
+    ["Hygor", "M"]
+]
+alert(pessoas[0][0]) 
+*/
+
+
+var pessoa = { nome: "Luana", cidade: "Brasília" }
+
+alert(pessoa.cidade)
+
+
+
+
+
+
+
+
+/*
+Multidimensional: []
+Associativo: {}
+*/
