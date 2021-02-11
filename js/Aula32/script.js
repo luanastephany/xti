ee195ead8c306d08fb47f706f4aae59d155637dd
Luana <ls.luanastephany@gmail.com>
2021-02-11 16:28:12 -0300

    Regular Expressions

---
+++ js/Aula32/script.js
@@ -0,0 +1,16 @@
+/*var regex = new RegExp("JavaScript")
+var string = "JavaScript"
+
+alert(regex.test(string)) //avaliar se existe ou não o padrão nesse conteúdo 
+*/
+
+
+
+
+/*var str = "Qual é o Doce mais doce que o doCe?"
+alert(/doce/ig.exec(str)) //o i ignora se é em maiúcula ou minúscula
+alert(str.match(/doce/ig)) //traz todas as referências que batem com o padrão */
+
+
+alert(/\w+@\w+\.\w{2,3}/.test("rh@harvarduniversity.com"))
+
