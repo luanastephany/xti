af0c4a157037bd0b8055aec9d40fc92fdd0a1822
Luana <ls.luanastephany@gmail.com>
2021-02-13 08:23:00 -0300

    Validando o email

--- js/Aula33/script.js
+++ js/Aula33/script.js
@@ -5,7 +5,9 @@ function validar() {
 
     //var re_cpf = /^([\d]{3})([\d]{3})([\d]{3})([\d]{2})$/
     var re_cpf = /[\d]{11}/
-    var re_email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z])+?$/ig
+
+    //var re_email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z])+?$/ig
+    var re_email = /^([\w-]+(\.[\w-]+)*)@(([\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(\.[a-z]{2})?)$/i
 
     if (re_cpf.test(cpf.value)) {
         alert("CPF válido")
@@ -13,7 +15,7 @@ function validar() {
         alert("CPF inválido")
     }
 
-    //verificando se o email é inválido
+    //verificando se o email é inválido. Se for válido, nada aparece
     if (!re_email.test(email.value)) {
         alert("Email inválido")
     }
