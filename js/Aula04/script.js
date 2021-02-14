82cfc5e437ac76b5b3396695ce5195687d4def02
Luana <ls.luanastephany@gmail.com>
2021-02-14 12:58:40 -0300

    Lampada (function)

--- js/Aula04/script.js
+++ js/Aula04/script.js
@@ -1,7 +1,18 @@
+var lampada = document.getElementById("lampada")
+
+
+/*lampada.onmouseover = function () {
+    lampada.src = "img/ligada.jpg"
+}*/
+
 function ligar() {
-    document.getElementById('lampada').src = "img/ligada.jpg"
+    lampada.src = "img/ligada.jpg"
 }
 
 function desligar() {
-    document.getElementById('lampada').src = "img/apagada.jpg"
+    lampada.src = "img/apagada.jpg"
 }
+
+lampada.onmouseover = ligar
+
+lampada.onmouseout = desligar
