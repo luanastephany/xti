cb59f7bd4e21cfe581cdcb564107b926c9020475
Luana <ls.luanastephany@gmail.com>
2021-02-15 07:43:28 -0300

    Browser Object Model

---
+++ js/Aula37/script.js
@@ -0,0 +1,7 @@
+//alert("Olá!")
+//confirm("Você quer ir para o Canadá?")
+//prompt("Para qual província você quer ir?")
+//window.open("../Aula14/imc.html", "imc")
+
+alert(screen.width)
+alert(screen.height)
