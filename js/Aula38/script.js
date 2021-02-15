072819fca503a1a6ad9b80616e5a351e67b8fd6b
Luana <ls.luanastephany@gmail.com>
2021-02-15 09:12:43 -0300

    Temporizador e hora real

---
+++ js/Aula38/script.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    document.getElementById("lampada").onclick = ligar
+    setInterval(apresentaHoras, 1000)
+}
+
+function ligar() {
+    document.getElementById("lampada").src = "img/ligada.jpg"
+    setTimeout("desligar()", 3000) //colocar função e intervalo em milissegundos
+}
+
+function desligar() {
+    document.getElementById("lampada").src = "img/apagada.jpg"
+}
+
+function apresentaHoras() {
+    var agora = new Date()
+    var horas = agora.getHours() + ":" + agora.getMinutes() + ":" + agora.getSeconds()
+    document.getElementById("horas").innerHTML = horas
+}
